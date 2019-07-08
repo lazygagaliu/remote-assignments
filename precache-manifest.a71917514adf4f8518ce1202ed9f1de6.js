4a8b80972ed6e2eae3053beeea0a0b124a90bd54
lazygagaliu <kidfly21@hotmail.com>
2019-07-08 18:44:14 +0800

    Updates

---
+++ precache-manifest.a71917514adf4f8518ce1202ed9f1de6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "38643257d2502469e75c84dde2592f8c",
+    "url": "/remote-assignments/week-4/assignment-3/layout/index.html"
+  },
+  {
+    "revision": "16ff477159f722ee0cbc",
+    "url": "/remote-assignments/week-4/assignment-3/layout/static/css/main.14e53650.chunk.css"
+  },
+  {
+    "revision": "43d1f55c2a696b4563b8",
+    "url": "/remote-assignments/week-4/assignment-3/layout/static/js/2.179f4c70.chunk.js"
+  },
+  {
+    "revision": "16ff477159f722ee0cbc",
+    "url": "/remote-assignments/week-4/assignment-3/layout/static/js/main.d29e98d1.chunk.js"
+  },
+  {
+    "revision": "357a43e2f9e2d561099c",
+    "url": "/remote-assignments/week-4/assignment-3/layout/static/js/runtime~main.e4e5c0b8.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/remote-assignments/week-4/assignment-3/layout/precache-manifest.a71917514adf4f8518ce1202ed9f1de6.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/remote-assignments/week-4/assignment-3/layout/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
